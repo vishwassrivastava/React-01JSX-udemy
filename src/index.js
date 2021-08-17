76cc84a277492a57ff1e18da552bac27fb132350
Vishwas Srivastava <vishwassrivastava1991@gmail.com>
2021-08-17 23:51:30 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import reactDOM from "react-dom";
+
+reactDOM.render(
+  <div>
+    <h1>hello world</h1>
+    <p>this is paragraph</p>
+    </div>,
+    document.getElementById("root")
+); 
